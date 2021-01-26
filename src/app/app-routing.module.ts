b16a04fd31c0dcc1291002b14eb66ce64bd830f5
volodymyrb <volodymyrb@fornova.net>
2021-01-26 14:50:35 +0200

    created pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import {RouterModule, Routes} from '@angular/router'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core'
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
@@ -12,11 +12,17 @@ const routes: Routes = [
             { path: 'post/:id', component: PostPageComponent },
         ]
     },
+    {
+        path: 'admin', loadChildren: () => import('./admin/admin.module')
+            .then(m => m.AdminModule),
+    },
 ]
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
