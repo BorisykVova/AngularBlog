0df7ea15f44d564fc0f7729f0168b15edf6dc8cd
volodymyrb <volodymyrb@fornova.net>
2021-01-26 14:57:49 +0200

    created pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component'
 import {AppRoutingModule} from './app-routing.module';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { PostPageComponent } from './post-page/post-page.component'
+import { PostPageComponent } from './post-page/post-page.component';
+import { PostComponent } from './shared/components/post/post.component'
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { PostPageComponent } from './post-page/post-page.component'
         AppComponent,
         MainLayoutComponent,
         HomePageComponent,
-        PostPageComponent
+        PostPageComponent,
+        PostComponent
     ],
     imports: [
         BrowserModule,
