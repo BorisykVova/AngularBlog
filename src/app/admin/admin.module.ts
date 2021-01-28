94669357ddb1c75b72be739296b1c480226d540f
volodymyrb <volodymyrb@fornova.net>
2021-01-28 12:16:03 +0200

    post form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,6 +9,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AuthService} from './shared/services/auth.service';
 import {AuthGuard} from './shared/services/auth.guard';
+import {QuillModule} from 'ngx-quill';
+import {SharedModule} from '../shared/shared.module';
 
 const routes: Routes = [
     {
@@ -27,6 +29,7 @@ const routes: Routes = [
 @NgModule({
     imports: [
         CommonModule,
+        SharedModule,
         RouterModule.forChild(routes),
         FormsModule,
         ReactiveFormsModule,
