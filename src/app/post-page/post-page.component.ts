272fcd9e716aa20df6954f97c0af56e30b994b19
volodymyrb <volodymyrb@fornova.net>
2021-02-06 23:11:36 +0200

    post detail page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {PostService} from '../shared/post.service';
+import {Observable} from 'rxjs';
+import {Post} from '../admin/shared/interfaces';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-post-page',
-  templateUrl: './post-page.component.html',
-  styleUrls: ['./post-page.component.scss']
+    selector: 'app-post-page',
+    templateUrl: './post-page.component.html',
+    styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+    post$: Observable<Post>
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private postService: PostService,
+    ) { }
+
+    ngOnInit(): void {
+        this.post$ = this.route.params.pipe(
+            switchMap((params: Params) => {
+                return this.postService.getById(params['id'])
+            }),
+        )
+    }
 
 }
